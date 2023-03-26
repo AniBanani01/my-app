3905dbbcfce17c56ac822abb297c8f71ada258cf
AniBanani01 <ani.parusheva11@gmail.com>
2023-03-26 20:32:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import '../src/reset.css'
+import '../src/App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+
     <App />
-  </BrowserRouter>
+  // </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
