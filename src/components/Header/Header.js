aa3b2a138e574c10add8b0bd862b0aadf5a5c377
AniBanani01 <ani.parusheva11@gmail.com>
2023-03-29 16:36:36 +0300

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+export const Header=()=>{
+    return(
+
+        <header>
+        <nav>
+            <ul>
+                <li className="menu-item home"><a href="#home">Home</a></li>
+                <li className="menu-item catalog"><a href="#catalog">Recipies</a></li>
+                <li className="menu-item about"><a href="#about">About us</a></li>
+                <li className="menu-item contacts"><a href="#contacts">Contacts</a></li>
+            </ul>
+
+
+            <ul>
+                <p>Welcome User</p>
+                <li className="menu-item details"><a href="#login">Login</a></li>
+                <li className="menu-item details"><a href="#register">Register</a></li>
+                <li className="menu-item details"><a href="#logout">Logout</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
+    );
+};
