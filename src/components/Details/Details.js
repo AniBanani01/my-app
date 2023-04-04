51eac980bc150ee3056924c2209f3f33ff176ea6
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-04 23:21:48 +0300

    Update Details.js

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,58 +1,119 @@
+import { useEffect, useState, useReducer } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+
+import { recipieServiceFactory } from '../../services/recipieService';
+import * as commentService from '../../services/commentService';
+import { useService } from '../../hooks/useService';
+import { useAuthContext } from '../../contexts/AuthContext';
+
+import { RecipieComment } from './RecipieComment/RecipieComment';
+import { recipieReducer } from '../../reducers/recipieReducer';
+import { useRecipieContext } from '../../contexts/RecipieContext';
+import { LikeButton } from './RcecipieLikes/RecipieLikes'
+
 export const Details=()=>{
-    return(
+    const { recipieId } = useParams();
+    const { userId, isAuthenticated, userEmail } = useAuthContext();
+    const { deleteRecipie } = useRecipieContext();
+    const [recipie, dispatch] = useReducer(recipieReducer, {});
+    const recipieService = useService(recipieServiceFactory)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Promise.all([
+            recipieService.getOne(recipieId),
+            commentService.getAll(recipieId),
+        ]).then(([recipieData, comments]) => {
+            const recipieState = {
+                ...recipieData,
+                comments,
+            };
+            
+            dispatch({type: 'RECIPIE_FETCH', payload: recipieState})
+        });
+    }, [recipieId]);
+  
+
+    // add coments
+    const onCommentSubmit = async (values) => {
+        const response = await commentService.create(recipieId, values.comment);
+
+        dispatch({
+            type: 'COMMENT_ADD',
+            payload: response,
+            userEmail,
+        });
+    };
+
+    const isOwner = recipie._ownerId === userId;
+
+    const onDeleteClick = async () => {
+        // eslint-disable-next-line no-restricted-globals
+        const result = confirm(`Are you sure you want to delete ${recipie.title}`);
+
+        if (result) {
+            await recipieService.delete(recipie._id);
+
+            deleteRecipie(recipie._id);
+
+            navigate('/recipies');
+        }
+    };
+
+
+return(
          
         <section className="recipie-details">
         <h1>Recipie Details</h1>
         <div className="info-section">
 
             <div className="recipie-header">
-                <img className="recipie-img" src="https://i.pinimg.com/474x/bb/16/7e/bb167e9ca1c4efeb5e7a3bab87dbbfab.jpg"  alt="img"/>
+                <img className="recipie-img" src={recipie.imageUrl} alt={recipie.title}/>
                 <div >
-                    <h1 className="recipie">Recipie name</h1>
-                    <p className="category">Under 5 ingredients</p>
-                    <p className="level">For beginers</p>
-                    <p className="cooking-time">15 min</p>
+                    <h1 className="recipie">{recipie.title}</h1>
+                    <p className="category">{recipie.category}</p>
+                    <p className="level">{recipie.level}</p>
+                    <p className="cooking-time">{recipie.time}</p>
                 </div>
                 <div>
                     <h1>INGREDIENTS</h1>
                     <ul>
-                        <li>Eggs</li>
-                        <li>Black pepper</li>
-                        <li>Garlic</li>
-                        <li>Pene</li>
+                        {recipie.ingredients &&( (recipie.ingredients).split(",")).map(i=>(
+                            <li key={i}>
+                                {i}
+                            </li>
+                        ))}
+                     
                     </ul>
                 </div>
             </div>
-            <div className="buttons">
-                <button className="button">Edit</button>
-                <button className="button">Delete</button>
-            </div>
+                    {isOwner && (
+                            <div className="buttons">
+                                {/* <Link to={`/recipies/${recipie._id}/edit`} className="button">Edit</Link> */}
+                                <button className="button">Edit</button>
+                                <button className="button" onClick={onDeleteClick}>Delete</button>
+                            </div>
+                        )}
 
-            <p className="text">ksknjkdfskdjnskjgfsnsgfnfgfjkgfkfgjgjfkgsnkjksjsgkangfjnkjfnfkfdkanjakvfnjj</p>
+            <p className="text">{recipie.text}</p>
 
             <div className="details-comments">
                 <h2>Comments:</h2>
                 <ul>
-                    <li className="comment">
-                        <p>Username: comment</p>
-                    </li>
-                </ul>
-                <article className="create-comment">
-                    <h2>Add new comment:</h2>
-                    <form className="form" >
-                        <textarea name="comment"
-                            placeholder="Comment......"
-                        ></textarea>
-                        <input
-                            className="btn submit"
-                            type="submit"
-                            value="Add Comment" />
-                    </form>
-                </article>
+                        {recipie.comments && recipie.comments.map(x => (
+                            <li key={x._id} className="comment">
+                                <p>{x.author.email}: {x.comment}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    {!recipie.comments?.length && (
+                        <p className="no-comment">No comments.</p>
+                    )}
             </div>
 
+            {isAuthenticated && <RecipieComment onCommentSubmit={onCommentSubmit} />   }
 
-
+        {/* <div><LikeButton/></div> */}
         </div>
 
     </section>
