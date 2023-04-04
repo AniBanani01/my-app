886db8e460a958b2d7d5d57e16f002fa082fbfbb
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-04 23:21:53 +0300

    Update Create.js

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,50 +1,65 @@
+// import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
+import {useRecipieContext} from '../../contexts/RecipieContext'
+
 export const Create=()=>{
+    const {onCreateRecipieSubmit} = useRecipieContext();
+    const { values, changeHandler, onSubmit } = useForm({
+        title: '',
+        imageUrl: '',
+        category: '',
+        ingredients:'',
+        time:'',
+        level:'',
+        text:'',
+    },onCreateRecipieSubmit);
+    
     return(
         <section className="create">
         <div className="form">
             <h2>New recipie</h2>
-            <form className="create-form">
-                <input
+            <form className="create-form" onSubmit={onSubmit}>
+                <input  value={values.title} onChange={changeHandler}
                     type="text"
                     name="title"
                     id="title"
                     placeholder="Recipie name"
                 />
-                <input
+                <input value={values.imageUrl} onChange={changeHandler}
                     type="text"
                     name="imageUrl"
                     id="recipie-image"
                     placeholder="Recipie Image"
                 />
-                <input
+                <input value={values.category} onChange={changeHandler}
                     type="text"
                     name="category"
                     id="recipie-category"
                     placeholder="Category"
                 />
 
-                <input
+                <input value={values.ingredients} onChange={changeHandler}
                     type="text"
                     name="ingredients"
                     id="ingredients"
                     placeholder="Ingredients"
                 />
-                <input
+                <input value={values.time} onChange={changeHandler}
                     type="text"
                     name="time"
                     id="time"
                     placeholder="Preparation Time"
                 />
-                <input
+                <input  value={values.level} onChange={changeHandler}
                     type="text"
                     name="level"
                     id="level"
                     placeholder="Level"
                 />
 
-                <textarea
+                <textarea value={values.text} onChange={changeHandler}
                     id="recipie-description"
-                    name="description"
+                    name="text"
                     placeholder="Preparation steps"
                     rows="5"
                     cols="50"
