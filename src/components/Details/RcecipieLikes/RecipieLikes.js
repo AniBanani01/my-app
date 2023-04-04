3a8693d670e4708666cae9af07bb4e8cbfa37548
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-04 23:21:51 +0300

    Create RecipieLikes.js

---
+++ src/components/Details/RcecipieLikes/RecipieLikes.js
@@ -0,0 +1,22 @@
+// import React, { useState } from 'react';
+
+
+// export const LikeButton = () => {
+//   const [likes, setLikes] = useState(0);
+//   const [isClicked, setIsClicked] = useState(false);
+
+//   const handleClick = () => {
+//     if (isClicked) {
+//       setLikes(likes - 1);
+//     } else {
+//       setLikes(likes + 1);
+//     }
+//     setIsClicked(!isClicked);
+//   };
+
+//   return (
+//     <button className={ `like-button ${isClicked && 'liked'}` } onClick={ handleClick }>
+//       <span className="likes-counter">{ `Like | ${likes}` }</span>
+//     </button>
+//   );
+// };
