0659ddcb23fdf17cd368390c7379f87eb02a4f66
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-04 23:22:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
+import { AuthProvider } from './contexts/AuthContext';
+import { RecipieProvider } from './contexts/RecipieContext';
 
 import './reset.css'
 import './App.css';
@@ -12,27 +14,31 @@ import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
 import { Recipies } from './components/Recipies/Recipies';
 import { Register } from './components/Register/Register';
+import {Logout} from './components/Logout/Logout'
 
 
 function App() {
+
     return (
-        <>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    {/* <Route path='/logout' element={<Logout />} /> */}
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/create-recipie' element={<Create />} />
-                    <Route path='/recipies' element={<Recipies />} />
-                    <Route path='/recipies/:recipieID' element={<Details />} />
-                </Routes>
-            </main>
 
-            <Footer />
+        <AuthProvider>
+            <RecipieProvider>
+                <Header />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/logout' element={<Logout />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/create-recipie' element={<Create />} />
+                        <Route path='/recipies' element={<Recipies />} />
+                        <Route path='/recipies/:recipieId' element={<Details />} />
+                    </Routes>
+                </main>
 
-        </>
+                <Footer />
+            </RecipieProvider>
+        </AuthProvider>
     );
 }
 
