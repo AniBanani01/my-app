3d6bdb0404b87c62d72215bc5ded0c2a0b8f9fe3
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-04 23:21:39 +0300

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,44 @@
+import { Link } from "react-router-dom"
+
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
+
+const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password'
+};
 
 
 export const Login=()=>{
+    const { onLoginSubmit } = useAuthContext();
+    const { values, changeHandler, onSubmit } = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: '',
+    }, onLoginSubmit);
+
     return(
         <section className="login">
         <div className="form">
             <h2>Login</h2>
-            <form className="login-form">
+            <form className="login-form" method="POST" onSubmit={onSubmit}>
                 <input
                     type="text"
-                    name="email"
                     id="email"
-                    placeholder="email" />
+                    placeholder="email" 
+                    name={LoginFormKeys.Email}
+                    value={values[LoginFormKeys.Email]}
+                    onChange={changeHandler}/>
                 <input
                     type="password"
-                    name="password"
                     id="password"
                     placeholder="password"
+                    name={LoginFormKeys.Password}
+                    value={values[LoginFormKeys.Password]}
+                    onChange={changeHandler}
                 />
                 <button type="submit">login</button>
                 <p className="message">
-                    Not registered? <a href="#">Create an account</a>
+                    Not registered? <Link to="/register">Create an account</Link>
                 </p>
             </form>
         </div>
