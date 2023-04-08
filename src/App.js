eb37369e046d28f4625799dbf35c3bf3c398bf98
AniBanani01 <ani.parusheva11@gmail.com>
2023-04-08 08:56:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import { Login } from './components/Login/Login';
 import { Recipies } from './components/Recipies/Recipies';
 import { Register } from './components/Register/Register';
 import {Logout} from './components/Logout/Logout'
+import { AboutUs } from './components/AboutUs/AboutUs';
+import { Contacts } from './components/Contacts/Contacts';
+import { Edit } from './components/Edit/Edit';
+
 
 
 function App() {
@@ -27,12 +31,16 @@ function App() {
                 <main>
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/contacts' element={<Contacts />} />
+                        <Route path='/about' element={<AboutUs />} />
                         <Route path='/login' element={<Login />} />
                         <Route path='/logout' element={<Logout />} />
                         <Route path='/register' element={<Register />} />
                         <Route path='/create-recipie' element={<Create />} />
                         <Route path='/recipies' element={<Recipies />} />
                         <Route path='/recipies/:recipieId' element={<Details />} />
+                        <Route path='/recipies/:recipieId/edit' element={<Edit />} />
+
                     </Routes>
                 </main>
 
